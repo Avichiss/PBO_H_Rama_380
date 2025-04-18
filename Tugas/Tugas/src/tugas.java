84da9e4deb72c5c2abea6ae383c381ae7d71cf89
Avichiss <bagusramanda81@gmail.com>
2025-04-18 20:33:22 +0700

    update tugas 3

--- Tugas/Tugas/src/tugas.java
+++ Tugas/Tugas/src/tugas.java
@@ -1,29 +1,32 @@
 import java.util.Scanner;
 
-class Admin {
-    String username = "Admin380";
-    String password = "password380";
+class User {
+    private String nama;
+    private String nim;
 
-    void login(String user, String pass) {
-        if (user.equals(username) && pass.equals(password)) {
-            System.out.println("Login Admin berhasil!");
-        } else {
-            System.out.println("Login gagal! Username atau password salah.");
-        }
+    public User(String nama, String nim) {
+        this.nama = nama;
+        this.nim = nim;
     }
-}
 
-class Mahasiswa {
-    String nama = "Ramanda Bagus Prabowo";
-    String nim = "202410370110380";
+    public String getNama() {
+        return nama;
+    }
 
-    void login(String inputNama, String inputNim) {
-        if (inputNama.equalsIgnoreCase(nama) && inputNim.equals(nim)) {
-            System.out.println("Login Mahasiswa berhasil!");
-            displayInfo();
-        } else {
-            System.out.println("Login gagal! Nama atau NIM salah.");
-        }
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public void login(String inputNama, String inputNim) {
+
     }
 
     void displayInfo() {
@@ -32,7 +35,53 @@ class Mahasiswa {
     }
 }
 
+class Admin extends User {
+    private String username = "Admin380";
+    private String password = "password380";
 
+    public Admin() {
+        super("Admin", "380");
+    }
+
+    @Override
+    public void login(String user, String pass) {
+        if (user.equals(username) && pass.equals(password)) {
+            System.out.println("Login Admin berhasil!");
+            displayInfo();
+        } else {
+            System.out.println("Login gagal! Username atau Password salah.");
+        }
+    }
+
+    @Override
+    void displayInfo() {
+        System.out.println("Nama: " + getNama());
+        System.out.println("NIM : " + getNim());
+        System.out.println("Login sukses!");
+    }
+}
+
+class Mahasiswa extends User {
+    public Mahasiswa() {
+        super("Ramanda Bagus Prabowo", "202410370110380");
+    }
+
+    @Override
+    public void login(String inputNama, String inputNim) {
+        if (inputNama.equalsIgnoreCase(getNama()) && inputNim.equals(getNim())) {
+            System.out.println("Login Mahasiswa berhasil!");
+            displayInfo();
+        } else {
+            System.out.println("Login gagal! Nama atau NIM salah.");
+        }
+    }
+
+    @Override
+    void displayInfo() {
+        System.out.println("Nama: " + getNama());
+        System.out.println("NIM : " + getNim());
+    }
+}
 
 public class tugas {
     public static void main(String[] args) {
